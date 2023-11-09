2cb043928985d533dc31afb427941f4dd1f945ba
KainanSilvaaSiqueiraa0 <115711921+CarlosFerreiraSilva@users.noreply.github.com>
2023-11-09 15:11:20 -0300

    a

---
+++ ExplorarTour/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExplorarTour.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
