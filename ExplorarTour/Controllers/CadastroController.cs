8bcc6e6403dafd33feccabf2eebd67d77f85bdbe
CarlosFerreiraSilva <115711921+CarlosFerreiraSilva@users.noreply.github.com>
2023-11-11 12:35:08 -0300

    A

---
+++ ExplorarTour/Controllers/CadastroController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExplorarTour.Controllers
+{
+    public class CadastroController : Controller
+    {
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+    }
+}
