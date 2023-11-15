63786ae56083d0e22516ee2289c1392a4ebbbab7
CarlosFerreiraSilva <115711921+CarlosFerreiraSilva@users.noreply.github.com>
2023-11-15 19:00:20 -0300

    a

---
+++ ExplorarTour/Controllers/DestinosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExplorarTour.Controllers
+{
+    public class DestinosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ExplorarTour/Controllers/FavoritosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExplorarTour.Controllers
+{
+    public class FavoritosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
