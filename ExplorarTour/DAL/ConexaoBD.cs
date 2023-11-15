5fe41be50d5ffe3e8f06131ce6597bd59b2d8700
CarlosFerreiraSilva <115711921+CarlosFerreiraSilva@users.noreply.github.com>
2023-11-15 17:12:09 -0300

    a

---
+++ ExplorarTour/DAL/ConexaoBD.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.SqlClient;
+
+namespace ExplorarTour.DAL
+{
+        public class ConexaoBD
+        {
+            private static SqlConnection Banco;
+            public static SqlConnection getConexao()
+            {
+                if (Banco == null)
+                {
+                    Banco = new SqlConnection(@"Server=GINELMA\SQLEXPRESS; Database=DBExploraTour;");
+                }
+
+                return Banco;
+            }
+        }
+    
+}
